abcf73c7893c53c91f360997e1173ac577e287e4
yrkrus <yrk.rus@gmail.com>
2023-08-23 16:27:18 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+/*
+	кинуть моентку
+	
+*/
+
+const int start{ 1 }; // орел
+const int stop{ 20 };  // решка
+
+
+void grewup(const int &result) 
+{
+	if (result > (stop/2)) 
+	{
+		cout << "Выпала: решка\n";
+	}
+	else 
+	{
+		cout << "Выпал: орел \n";
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+
+	int monetka;	
+	char choise='0';
+
+
+	while (choise != 'q')
+	{
+		monetka = rand() % stop + start;
+		grewup(monetka);
+
+		cout << "Для повтора нажать любую клавишу. Для выхода нажать клавишу q\n";
+		cin >> choise;
+	};
+}
+
